2f48d4ba0cbe1dcfebe79cee0d7ebb8b72badc61
Simona Bliuvaite <bliuv.simona@gmail.com>
2025-01-20 14:43:02 +0200

    updating threat information

--- Frontendas/models/SecurityThreat.js
+++ Frontendas/models/SecurityThreat.js
@@ -3,17 +3,6 @@ export class SecurityThreat {
         this.Id = data.id; 
         this.Title = data.title;
         this.Description = data.description;
-        //this.FirstOccured = data.firstOccured;
-        //this.LastOccured = data.lastOccured;
-        //this.DateAdded = data.dateAdded;
-        this.Severity = data.severity;
-        this.Status = data.status;
-        this.Category = data.category;
-        this.Source = data.source;
-        //this.RelatedEvent = data.RelatedEvent;
-        this.Visibility = data.visibility;
-        //this.User = data.User;
-        this.Impact = data.impact;
-        this.Response = data.response;
+        this.RelatedEvent = data.RelatedEvent;
     }
 }
